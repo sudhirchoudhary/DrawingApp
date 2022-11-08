bfb3e9126954d148139e41cd03b7993c5ed3b84c
Sudhir Choudhary <sudhir.choudhary@lybrate.com>
2022-11-08 15:14:04 +0530

    fixes

---
+++ app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -0,0 +1,47 @@
+package com.example.drawingapp
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+
+class DrawingView(context: Context, attrs: AttributeSet): View(context, attrs) {
+    private var mDrawPath: CustomPath? = null
+    private var mCanvasBitmap: Bitmap? = null
+    private var mDrawPaint: Paint? = null
+    private var mCanvasPaint: Paint? = null
+    private var mBrushSize: Float = 0f
+    private var color = Color.BLACK
+    private var canvas: Canvas? = null
+
+    init {
+        setUpDrawing()
+    }
+
+    private fun setUpDrawing() {
+        mDrawPaint = Paint()
+        mDrawPath = CustomPath(color, mBrushSize)
+
+        mDrawPaint!!.apply {
+            color = this@DrawingView.color
+            style = Paint.Style.STROKE
+            strokeJoin = Paint.Join.ROUND
+            strokeCap = Paint.Cap.ROUND
+        }
+
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+        mBrushSize = 20f
+
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+
+    internal inner class CustomPath(var color: Int, var brushThickness: Float): Path() {
+
+    }
+}
